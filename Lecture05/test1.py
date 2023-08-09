6b375f6305c0147c26b417ee64684896e43db6f6
beam2711 <beam34180@gmail.com>
2023-08-09 14:27:30 +0700

    bema

---
+++ Lecture05/test1.py
@@ -0,0 +1,12 @@
+#def main():
+ #   print('I Love Python')
+   # print()
+    #print('My Lucky No. is 7')
+#main()
+#ss= "I Love Python"
+#print(ss[:4]) 7 vowels
+massage = (0)
+string = input(str('Enter your massage:'))
+print(string)
+def main():
+    globals massage
