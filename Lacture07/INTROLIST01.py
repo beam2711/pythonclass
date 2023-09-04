f0827a81e526ec928506ceb519a0d00aa94c24a0
beam2711 <beam34180@gmail.com>
2023-09-04 11:37:56 +0700

    111

--- Lacture07/INTROLIST01.py
+++ Lacture07/INTROLIST01.py
@@ -1,8 +1,13 @@
 even_numbers = [2, 4, 6, 8, 10]
 horoes = ['Ironman', 'Thor', 'hulk', 'Spiderman']
 info = ['Batman', 4500, 6375.60]
-number = list(range(5))
+numbers = list(range(5))
 numlisst = [4] * 5
 dlist = [1, 2, 3] * 3
 
-##
+print(even_numbers)
+print(horoes)
+print(info)
+print(numbers)
+print(numlisst)
+print(dlist)
---
+++ Lacture07/LISTOPERATOR01.py
@@ -0,0 +1,11 @@
+ever_number = [2, 4, 6, 8, 10]
+heroes = ['Ironman', 'Thor', 'hulk', 'Spiderman']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(numbers[-5:])
+numbers[8] = 99
+print(numbers)
+
+pluslist = heroes + ever_number
+print(pluslist)
+print(len(numbers))
