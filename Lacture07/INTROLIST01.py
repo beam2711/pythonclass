12cd269df060326a64a61c6bc4f98ef95d450945
beam2711 <beam34180@gmail.com>
2023-09-04 09:51:06 +0700

    11111

---
+++ Lacture07/INTROLIST01.py
@@ -0,0 +1,7 @@
+even_numbers = [2, 4, 6, 8, 10]
+horoes = ['Ironman', 'Thor', 'hulk', 'Spiderman']
+info = ['Batman', 4500, 6375.60]
+number = list(range(5))
+numlisst = [4] * 5
+dlist = [1, 2, 3] * 3
+
